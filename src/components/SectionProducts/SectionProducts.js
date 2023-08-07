b48de31e218e8ec0948b1df11db3038f1a331464
lukasilverio94 <lukasilverio94@gmail.com>
2023-08-07 16:45:31 +0200

    SectionProducts finalizada

--- src/components/SectionProducts/SectionProducts.js
+++ src/components/SectionProducts/SectionProducts.js
@@ -55,13 +55,15 @@ export default function SectionProducts() {
           </div>
         </div>
         {/* Features Products*/}
-        <p>Todos os nossos produtos incluem: </p>
-        <ul>
-          <li>Garantia de 1 ano</li>
-          <li>Manutenção preventiva</li>
-          <li>Descontos especiais na compra da segunda unidade</li>
-          <li>Flexibilidade de pagamento</li>
-        </ul>
+        <div className="products_features">
+          <p>Todos os nossos produtos incluem: </p>
+          <ul>
+            <li>Garantia de 1 ano</li>
+            <li>Manutenção preventiva</li>
+            <li>Descontos especiais na compra da segunda unidade</li>
+            <li>Flexibilidade de pagamento</li>
+          </ul>
+        </div>
       </div>
     </section>
   );
