df03b69c7c6142089804615eb127ecaad43ab0ea
lukasilverio94 <lukasilverio94@gmail.com>
2023-08-08 10:34:35 +0200

    finish footer css

--- src/components/ContactSection/ContactSection.js
+++ src/components/ContactSection/ContactSection.js
@@ -1,29 +1,43 @@
+import "./ContactSection.css";
+
 const ContactSection = () => {
-return (
-  <section id="contacts">
+  return (
+    <section id="contacts">
       <div className="limit_container">
         <h1>FALE CONOSCO</h1>
-        <p>Não perca tempo, venha conhecer uma de nossas lojas ou entre em contato através de nossas redes sociais ou da central de atendimento.</p>
-      </div>
-      {/*Container dos contatos dentro tem mais 2 pequenos  */}
-      <div className="contact_container">
-        <div className="contact_box">
-          <h3>Contato</h3>
-          {/*INSERIR ICONES DEPOIS*/}
-          <p>Nova Iguaçu, RJ</p>
-          <p>(21) 9999-9999</p>
-          <p>contato@oticavida.com</p>
+        <p>
+          Não perca tempo, venha conhecer uma de nossas lojas ou entre em
+          contato através de nossas redes sociais ou da central de atendimento.
+        </p>       
+        <div className="contact_container">
+          <div className="contact_box">
+            <h3>Contato</h3>            
+            <div>
+              <p><img src="assets/local.png"/>Nova Iguaçu, RJ</p>
+            </div>
+            <div>
+              <p><img src="assets/telefone.png"/>(21) 9999-9999</p>
+            </div>
+            <div>
+              <p><img src="assets/local.png"/>contato@oticavida.com</p>
+            </div>
+          </div>
+          <div className="contact_box">
+            <h3>Nossas Redes Sociais</h3>
+            <div>
+              <p><img src="assets/fb.png"/>OticaVida</p>
+            </div>
+            <div>
+              <p><img src="assets/ig.png"/>@oticavidarj</p>
+            </div>
+            <div>
+              <p><img src="assets/ig.png"/>@oticavidarj</p>
+            </div>
+          </div>
         </div>
-        <div className="social_box">
-          <h3>Nossas Redes Sociais</h3>
-          <p>/OticaVida</p>
-          <p>@oticavidarj</p>
-          <p>@oticavidarj</p>
-        </div>
       </div>
+    </section>
+  );
+};
 
-  </section>
-)
-}
-
-export default ContactSection
+export default ContactSection;
